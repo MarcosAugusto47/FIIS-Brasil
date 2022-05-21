5c1e9442f23e6a91c054214dab817001ea6a2352
MarcosAugusto47 <marcosbsb2014@gmail.com>
2022-05-21 17:19:33 -0300

    some changes on utils folder and notebook

--- notebooks/utils/functions.py
+++ notebooks/utils/functions.py
@@ -1,7 +1,26 @@
-def convert_string_to_float(df, colnames):
-    for colname in colnames:
-        df[colname] = df[colname].str.replace('R$', "", regex=False)
-        df[colname] = df[colname].str.replace('.', "", regex=False)
-        df[colname] = df[colname].str.replace(',', ".", regex=False)
-        df[colname] = df[colname].apply(lambda x: float(x))
-    return df
+from bs4 import BeautifulSoup
+from urllib.request import Request, urlopen
+
+def trata_html(input):
+   return " ".join(input.split()).replace('> <', '><')
+
+def create_bsoup_object(url):
+    headers = {'User-Agent': 'xxxx'}
+    req = Request(url, headers = headers)
+    response = urlopen(req)
+    html = response.read()
+    html = html.decode('utf-8')
+    html = trata_html(html)
+    ## create BeautifulSoup object
+    soup = BeautifulSoup(html, 'html.parser')
+    return soup
+
+
+def get_tickers(soup):
+    tickets = soup.findAll('span', {'class': 'ticker'})
+    codigos = []
+    
+    for ticket in tickets:
+        codigos.append(ticket.get_text().lower())
+    
+    return codigos
---
+++ notebooks/utils/helpers.py
@@ -0,0 +1,27 @@
+import numpy as np
+import pandas as pd
+def convert_string_to_float(df, colnames):
+    for colname in colnames:
+        df[colname] = df[colname].str.replace('R$', "", regex=False)
+        df[colname] = df[colname].str.replace('.', "", regex=False)
+        df[colname] = df[colname].str.replace(',', ".", regex=False)
+        df[colname] = df[colname].apply(lambda x: float(x))
+    return df
+
+def convert_percentages_to_float(df, colnames):
+    for colname in colnames:
+            df[colname] = df[colname].str.replace('R$', "", regex=False)
+            df[colname] = df[colname].str.replace('.', "", regex=False)
+            df[colname] = df[colname].str.replace(',', ".", regex=False)
+            df[colname] = df[colname].str.replace('%', "", regex=False)
+            df[colname] = df[colname].apply(lambda x: float(x))
+            df[colname] = df[colname] / 100
+
+    return df
+
+def convert_to_datetime(x):
+    try:
+        x = pd.to_datetime(x, format = "%d/%m/%Y")
+    except ValueError:
+        x = np.nan
+    return x

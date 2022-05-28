65268c539d6d77e83e2d2ea731788563ec86a0a4
MarcosAugusto47 <marcosbsb2014@gmail.com>
2022-05-28 12:50:29 -0300

    new files organization and some code improving

--- script-scraping.py
+++ script-scraping.py
@@ -1,99 +1,30 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-Este é um arquivo de script temporário.
-"""
-
+# libraries
 import re 
-
-import bs4
-import urllib.request as urllib_request
+import datetime
 import pandas as pd
-print("BeautifulSoup ->", bs4.__version__)
-print("urllib ->", urllib_request.__version__)
-print("pandas ->", pd.__version__)
 
-from urllib.request import Request, urlopen
+import urllib.request as urllib_request
 from urllib.error import URLError, HTTPError
-from bs4 import BeautifulSoup
 
-def trata_html(input):
-    return " ".join(input.split()).replace('> <', '><')
+from utils.helpers import *
+from utils.functions import *
 
-## Rotina de scraping
-### Declara-se a variável cards
-cards = []
-
-###
-url = 'https://fiis.com.br/lista-de-fundos-imobiliarios/'
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:79.0) Gecko/20100101 Firefox/79.0'}
-req = Request(url, headers = headers)
-response = urlopen(req)
-html = response.read()
-html = html.decode('utf-8')
-html = trata_html(html)
-
-## Criação de um objeto Beautiful Soup
-soup = BeautifulSoup(html, 'html.parser')
-# print(soup.prettify())
-tickets = soup.findAll('span', {'class': 'ticker'})
-codigos = []
-for ticket in tickets:
-    codigos.append(ticket.get_text().lower())
-        
-for codigo in codigos:
-    url = 'https://fiis.com.br/' + codigo
-    headers = {'User-Agent': 'xxxx'} # olhar no seu PC
-    req = Request(url, headers = headers)
-    response = urlopen(req)
-    html = response.read()
-    html = html.decode('utf-8')
-    html = trata_html(html)
-    soup = BeautifulSoup(html, 'html.parser')
-    items = soup.findAll('span', {'class': 'value'})
-    infos = []
-    card = {}
-    for item in items:
-        infos.append(item.get_text())
-    card['Dividend Yield'] = infos[0]
-    card['Ultimo Rendimento'] = infos[1]
-    card['Patrimonio Liquido'] = infos[2]
-    card['Valor Patrimonial por Cota'] = infos[3]
-    card['Site'] = infos[6]
-    card['Codigo'] = infos[7]
-    card['Tipo do FII'] = infos[8]
-    card['Registro CVM'] = infos[10]
-    card['Numero de Cotas'] = infos[11]
-    card['Numero de Cotistas'] = infos[12]
-    card['Ticker'] = codigo.upper()
-    
-    admin = soup.findAll("div", {"class": "text-wrapper"})
-    card['Administrador'] = re.sub("ADMINISTRADOR|[0-9].*[0-9]", "", admin[0].get_text())
-    
-    cotacao = soup.findAll("div", {"class": "item quotation"})
-    cotacao = cotacao[0].find("span", {"class": "value"}).get_text()
-    
-    min52 = soup.findAll("div", {"class": "item min52"})
-    min52 = min52[0].find("div", {"class": "value"}).get_text()
-
-    max52 = soup.findAll("div", {"class": "item max52"})
-    max52 = max52[0].find("div", {"class": "value"}).get_text()
-
-    valorizacao12 = soup.findAll("div", {"class": "item val12"})
-    valorizacao12 = valorizacao12[0].find("div", {"class": "value"}).get_text()
-    
-    card["Cotacao Atual"] = cotacao
-    card["Min. 52 semanas"] = min52
-    card["Max. 52 semanas"] = max52
-    card["Valorizacao"] = valorizacao12
-    
-    cards.append(card)
-
-## Cria-se um dataframe com os resultados
-dataset = pd.DataFrame(cards)
-dataset = dataset.apply(lambda x: x.str.replace("R\\$|\\.","", regex = True).str.replace(",", ".").astype(float) if x.name in ["Ultimo Rendimento", "Valor Patrimonial por Cota", "Numero de Cotas", "Numero de Cotistas", "Cotacao Atual","Min. 52 semanas", "Max. 52 semanas"] else x)
-dataset["P/VP"] = dataset['Cotacao Atual'] / dataset['Valor Patrimonial por Cota']
-dataset.to_csv('C:/Users/marco/Desktop/Estatistica/fiis_dataset.csv',
-               sep = ';',
-               index = False)
+# procedure
+URL = 'https://fiis.com.br/lista-de-fundos-imobiliarios/'
+soup = create_bsoup_object(URL)
+tickers = get_tickers(soup)
+dataset = create_fiis_dataframe(tickers)
+COLUMNS_LIST = ['ultimo_rendimento',
+                'valor_patrimonial_por_cota',
+                'numero_cotas',
+                'numero_cotistas',
+                'cotacao',
+                'min_52_weeks',
+                'max_52_weeks']
+dataset = convert_string_to_float(dataset, COLUMNS_LIST)
+dataset = convert_percentages_to_float(dataset, ['dividend_yield', 'return_12_months'])
+dataset = break_tipo_FII(dataset)
+dataset['registro_CVM'] = dataset['registro_CVM'].apply(lambda x: str(convert_to_datetime(x)))
+#dataset["p_vp"] = (dataset['cotacao'] / dataset['valor_patrimonial_por_cota']).replace(np.inf, np.nan)
+dataset['date'] = str(datetime.date.today())
+print(dataset.head())
---
+++ utils/functions.py
@@ -0,0 +1,91 @@
+import json
+import pandas as pd
+import re
+from decimal import Decimal
+from bs4 import BeautifulSoup
+from urllib.request import Request, urlopen
+
+import regex
+
+def trata_html(input):
+   return " ".join(input.split()).replace('> <', '><')
+
+def create_bsoup_object(url):
+    headers = {'User-Agent': 'xxxx'}
+    req = Request(url, headers = headers)
+    response = urlopen(req)
+    html = response.read()
+    html = html.decode('utf-8')
+    html = trata_html(html)
+    ## create BeautifulSoup object
+    soup = BeautifulSoup(html, 'html.parser')
+    return soup
+
+
+def get_tickers(soup):
+    tickets = soup.findAll('span', {'class': 'ticker'})
+    codigos = []
+    
+    for ticket in tickets:
+        codigos.append(ticket.get_text().lower())
+    
+    return codigos
+
+def create_fiis_dataframe(tickers):
+    cards = []
+    for ticker in tickers:
+        print(ticker.upper())
+        url = 'https://fiis.com.br/' + ticker
+        soup = create_bsoup_object(url)
+        items = soup.find_all(class_ = 'value')
+        infos = []
+        card = {}
+        for item in items:
+            infos.append(item.get_text())
+        card['ticker'] = ticker.upper()
+        admin = soup.findAll("div", {"class": "text-wrapper"})
+        card['administrador'] = re.sub("ADMINISTRADOR|[0-9].*[0-9]", "", admin[0].get_text())
+        card['dividend_yield'] = infos[0]
+        card['ultimo_rendimento'] = infos[1]
+        card['patrimonio_liquido'] = infos[2]
+        card['valor_patrimonial_por_cota'] = infos[3]
+        card['telefone'] = infos[4]
+        card['email'] = infos[5]
+        card['site'] = infos[6]
+        card['name'] = infos[7]
+        card['tipo_FII'] = infos[8]
+        card['tipo_ANBIMA'] = infos[9]
+        card['registro_CVM'] = infos[10]
+        card['numero_cotas'] = infos[11]
+        card['numero_cotistas'] = infos[12]
+        card['CNPJ'] = infos[13]
+        card['cotacao'] = infos[14]
+        card['min_52_weeks'] = infos[15]
+        card['max_52_weeks'] = infos[16]
+        card['return_12_months'] = infos[17]
+            
+        cards.append(card)
+        
+    ## Create dataframe with the results
+    dataset = pd.DataFrame(cards)
+    return dataset
+
+def break_tipo_FII(dataset):
+    dataset["tipo"] = dataset['tipo_FII'].str.replace(":.*", "", regex = True).str.strip()
+    dataset["subtipo"] = dataset['tipo_FII'].str.replace(".*:", "", regex = True).str.strip()
+    dataset = dataset.drop(['tipo_FII'], axis='columns')
+    return dataset
+
+def batch_write(dynamodb, table_name, items):
+    """
+    Batch write items to given table name
+    """
+       
+    table = dynamodb.Table(table_name)
+    
+    with table.batch_writer() as batch:
+        for item in items:
+            #print(item)
+            item = json.loads(json.dumps(item), parse_float=Decimal)
+            batch.put_item(Item=item)
+    return True
